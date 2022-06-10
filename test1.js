5f7ea07d5e8bb8d3666efa2730164d13b536f78a
shirelyhou0130 <h2772489816@163.com>
2022-06-10 22:40:12 +0800

    add

---
+++ test1.js
@@ -0,0 +1,7 @@
+// var user = new Object();
+var user = {};
+user.name = "John";
+user.surname = "Mike";
+user.name = "Peter";
+delete user.name;
+console.log(user);
---
+++ test2.js
@@ -0,0 +1,7 @@
+var fruit = {
+  apple: 20,
+  pear: 20,
+  peach: 10
+  };
+  var res = fruit.apple+fruit.pear+fruit.peach;
+ console.log("res:"+res);
